ab599af1c4bd2f94188ac8e0d63c30e48a5110e5
Nguyễn Huy San <144963803+san601@users.noreply.github.com>
2024-03-10 11:28:23 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -108,7 +108,7 @@ void mergeSort(float arr[], int l, int r)
         int m = l + (r - l) / 2;
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
-        merge(arr, l, m, r);
+        if (arr[m] > arr[m + 1]) merge(arr, l, m, r);
     }
 }
 
